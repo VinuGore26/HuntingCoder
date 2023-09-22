95b31eb9fb1bc46816df2cf34120a5098d775ff6
VinuGore26 <vvgore2675@gmail.com>
2023-09-22 22:45:54 +0530

    Adding files with the start of api learning.

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import styles from "@/styles/Home.module.css";
 
 const blog = () => {
@@ -6,6 +7,33 @@ const blog = () => {
     <>
       <main className={styles.main}>
         <h1>This is an Blog page.</h1>
+        <div className={styles.blogs}>
+          <div className={styles.blogItem}>
+            <Link href={'/blogPost/learn-javasvript'}>
+              <h3 className={styles.headLine}>
+                How to learn Javascript in 2023
+              </h3>
+            </Link>
+            <p className={styles.para}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+              quidem impedit sed reprehenderit, praesentium illo obcaecati?
+            </p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3 className={styles.headLine}>How to learn Javascript in 2023</h3>
+            <p className={styles.para}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+              quidem impedit sed reprehenderit, praesentium illo obcaecati?
+            </p>
+          </div>
+          <div className={styles.blogItem}>
+            <h3 className={styles.headLine}>How to learn Javascript in 2023</h3>
+            <p className={styles.para}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+              quidem impedit sed reprehenderit, praesentium illo obcaecati?
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
